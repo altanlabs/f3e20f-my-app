542a2324949b0a7811b6763eb3d4d504f4ab998a
altanlabs <interfaces@altan.ai>
2025-02-08 12:53:57 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,26 @@
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "@/theme/theme-provider";
-import RootBoundary from "./components/errors/RootBoundary";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './layout'
+import IndexPage from './pages'
+import TagsPage from './pages/tags'
+import ComponentsPage from './pages/components'
+import NotFound from './pages/NotFound'
+import { ThemeProvider } from '@/theme/theme-provider'
 
-import Layout from "./layout";
-import Index from "./pages/index";
-import NotFound from "./pages/NotFound";
-import { useTheme } from "./theme/use-theme";
-
-const App = () => {
-  const { theme } = useTheme();
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <Layout showSidebar={false} showHeader={false} showFooter={false} />
-      ),
-      errorElement: <RootBoundary />,
-      children: [
-        {
-          index: true,
-          element: <Index />,
-        },
-        {
-          path: "*",
-          element: <NotFound />,
-        },
-      ],
-    },
-  ]);
-
+function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Theme appearance={theme === "system" ? "light" : theme}>
-        <div className={theme}>
-          <RouterProvider router={router} />
-        </div>
-      </Theme>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path="tags" element={<TagsPage />} />
+            <Route path="components" element={<ComponentsPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
